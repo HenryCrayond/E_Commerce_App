507ee78af3c7e0842e4046d45fd162d208f903ac
Henry <henry@crayond.co>
2023-10-05 13:21:04 +0530

    completed task

--- src/store/productStore.tsx
+++ src/store/productStore.tsx
@@ -1,51 +1,58 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { create } from "zustand";
 import { AllProduct, ProductItems } from "../App";
-
-interface Counter{
-    count:number;
-    addCount:(by:number)=>void;
+interface Counter {
+  count: number;
+  addCount: (by: number) => void;
 }
 
 interface ProductStore {
-    products:any;
-    addProduct:(item:ProductItems)=>void;
-    deleteProduct:(id:number)=>void;
-    updateProduct:(item:ProductItems,index:number)=>void;
+  products: any;
+  addProduct: (item: ProductItems) => void;
+  deleteProduct: (id: number) => void;
+  updateProduct: (item: ProductItems, index: number) => void;
 }
-interface GetProduct{
-    storeProduct:AllProduct; 
-    getFetchData:(item:ProductItems)=>void; 
+interface GetProduct {
+  storeProduct: AllProduct;
+  getFetchData: (item: ProductItems) => void;
 }
-interface Product{
-    id?:number;
-    price?:number;
-    rating?:number;
-    stock?:number;
-    brand?:string;
-    title?:string;
-    thumbnail?:string;
-    category?:string;
-    description?:string;
-    discountPercentage?:number;
-    images?:string[]
+interface Product {
+  id?: number;
+  price?: number;
+  rating?: number;
+  stock?: number;
+  brand?: string;
+  title?: string;
+  thumbnail?: string;
+  category?: string;
+  description?: string;
+  discountPercentage?: number;
+  images?: string[];
 }
 
-export const getAllProductStore = create<GetProduct>((set)=>({
- storeProduct:[],
- getFetchData:(products:Product)=>set(()=>({storeProduct:products})),
+export const getAllProductStore = create<GetProduct>((set) => ({
+  storeProduct: [],
+  getFetchData: (products: Product) => set(() => ({ storeProduct: products })),
+}));
 
-}))
+export const productStore = create<ProductStore>((set) => ({
+  products: [],
+  addProduct: (products: Product) =>
+    set((prev: ProductStore) => ({ products: [...prev.products, products] })),
+  deleteProduct: (id: number) =>
+    set((prev: ProductStore) => ({
+      products: prev.products.filter((pro: ProductItems) => pro?.id !== id),
+    })),
+  updateProduct: (products: Product, i: number) =>
+    set((prev: ProductStore) => ({
+      products: prev.products.filter((val: AllProduct) =>
+        val.id === products.id ? prev.products[i] : products
+      ),
+    })),
+}));
 
-export const productStore = create<ProductStore>((set)=>({
-    products:[],
-    addProduct:(products:Product)=>set((prev:ProductStore)=>({products:[...prev.products,products]})),
-    deleteProduct:(id:number)=>set((prev:ProductStore)=>({products:prev.products.filter((pro:ProductItems)=>pro?.id !== id)})),
-    updateProduct:(products:Product,i:number)=>set((prev:ProductStore)=>({products:prev.products.filter((val:AllProduct)=>val.id===products.id ? prev.products[i]:products)}))
-}))
-
-
-export const showCountStore = create<Counter>((set)=>({
-    count:3,
-    addCount:(by:number)=>set((prev:Counter)=>({count:prev.count + by}))
-}))  
+export const showCountStore = create<Counter>((set) => ({
+  count: 3,
+  addCount: (by: number) =>
+    set((prev: Counter) => ({ count: prev.count + by })),
+}));
